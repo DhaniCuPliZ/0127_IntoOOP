c7447c179d9009ad5f293ba6ea23a66e4169227b
DhaniCuPliZ <ramadhanirafli065@gmail.com>
2024-04-22 14:40:31 +0700

    Tambahkan proses input untuk kode MK, nama MK, dan SK

--- PemdasOOP2/PemdasOOP2.cpp
+++ PemdasOOP2/PemdasOOP2.cpp
@@ -23,6 +23,11 @@ class Matakuliah {
 		int sks;
 	public:
 		void input() {
-
+			cout << "Kode MK: ";
+			cin >> kodeMk;
+			cout << "Nama MK: ";
+			cin >> namaMk;
+			cout << "SKS: ";
+			cin >> sks;
 		}
 };
